c350221fa94f080728139227da84be40ffcbf433
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2020-01-11 14:55:19 +0100

    propagate thread IDs

--- utils/threads.sql
+++ utils/threads.sql
@@ -1,20 +1,13 @@
--- recursively assign thread ID and calculate the height
+-- Add thread IDs to comments that have none. This does work on
+-- PostgreSQL, but not on SQLite3.
 WITH RECURSIVE
-     dis (id, parent, thread, height, name, title) AS (
-     SELECT id, parent, id, 0, name, title FROM comments WHERE parent is NULL
+     dis (comment_id, id, parent, newthread) AS (
+     SELECT comment_id, id, parent, id FROM comment
+     WHERE parent is NULL AND thread is NULL
      UNION
-     SELECT comments.id, comments.parent, dis.thread, dis.height+1, comments.name, comments.title
-     FROM comments, dis WHERE comments.parent = dis.id)
-SELECT * FROM dis;
-
-
--- Same as above, but regenerate the whole table. See threadview.sql
--- for a view that stores this query.
-WITH RECURSIVE
-     dis (id, domain, text, title, user, name, date_informal, date, parent, thread, up_votes, down_votes, url, scrape_url, scraper, height) AS (
-     SELECT id, domain, text, title, user, name, date_informal, date, parent, id, up_votes, down_votes, url, scrape_date, scraper, 0 FROM comments WHERE parent is NULL
-     UNION
-     SELECT comments.id, comments.domain, comments.text, comments.title, comments.user, comments.name, comments.date_informal, comments.date, comments.parent, dis.thread, comments.up_votes, comments.down_votes, comments.url, comments.scrape_date, comments.scraper, dis.height+1
-     FROM comments, dis WHERE comments.parent = dis.id)
-SELECT * FROM dis;
-
+     SELECT comment.comment_id, comment.id, comment.parent, dis.newthread
+     FROM comment, dis
+     WHERE comment.parent = dis.id AND comment.thread is NULL)
+UPDATE comment SET thread = dis.newthread
+FROM dis
+WHERE comment.comment_id = dis.comment_id;
--- utils/threadview.sql
+++ utils/threadview.sql
@@ -1,5 +1,12 @@
--- create a view on comments, where thread IDs are present
-CREATE VIEW comment
+-- This creates a view named comments on the comment table. It
+-- generates missing thread IDs recursively. It works on SQLite3 and
+-- even with PostgreSQL, but for PostgreSQL you should use threads.sql
+-- to propagate the thread IDs directly into the comment table.
+--
+-- USAGE with sqlite3:
+--
+-- sqlite3 data.db < threadview.sql
+CREATE VIEW comments
 -- This is a view on comments with thread IDs propagated.
 AS
 WITH RECURSIVE
@@ -7,28 +14,27 @@ WITH RECURSIVE
      	  domain,
 	  text,
 	  title,
-	  user,
+	  user_id,
 	  name,
 	  date_informal,
 	  date,
+	  article_id,
 	  parent,
 	  thread,
 	  up_votes,
 	  down_votes,
-	  url,
-	  scrape_url,
-	  scraper,
+	  url_id,
 	  height)
      AS (
-     SELECT id, domain, text, title, user, name, date_informal, date,
-     	    parent, id, up_votes, down_votes, url, scrape_date, scraper, 0
-	    FROM comments WHERE parent is NULL
+     SELECT id, domain, text, title, user_id, name, date_informal, date, article_id,
+     	    parent, id, up_votes, down_votes, url_id, 0
+	    FROM comment WHERE parent is NULL
      UNION
-     SELECT comments.id, comments.domain, comments.text, comments.title,
-     	    comments.user, comments.name, comments.date_informal,
-	    comments.date, comments.parent, dis.thread, comments.up_votes,
-	    comments.down_votes, comments.url, comments.scrape_date,
-	    comments.scraper, dis.height+1
-     FROM comments, dis WHERE comments.parent = dis.id)
+     SELECT comment.id, comment.domain, comment.text, comment.title,
+     	    comment.user_id, comment.name, comment.date_informal,
+	    comment.date, comment.article_id,
+	    comment.parent, dis.thread, comment.up_votes,
+	    comment.down_votes, comment.url_id, dis.height+1
+     FROM comment, dis WHERE comment.parent = dis.id)
 SELECT * FROM dis
 GO
